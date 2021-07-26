ac10e22c3f0964edce9ccf1503651c60c0ede755
akashz19 <anand.btech.cs18@iiitranchi.ac.in>
2021-07-27 02:39:50 +0530

    Changes Made

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route} from "react-router-dom";
+
+import Navbar from "./components/navbar.component"
+import TaskList from "./components/tasks-list.component";
+import EditTask from "./components/edit-task.component";
+import CreateTask from "./components/create-task.component";
+import CreateUser from "./components/create-user.component";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <br/>
+      <Route path="/" exact component={TaskList} />
+      <Route path="/edit/:id" component={EditTask} />
+      <Route path="/create" component={CreateTask} />
+      <Route path="/user" component={CreateUser} />
+    </Router>
   );
 }
 
